9deb9a5e6228f605cc08f5ea6b048e38ec440d13
Ken Lovely <klovely@ccmg.com>
2019-09-09 13:29:01 -0400

    got working

---
+++ Cosmic.WebApp/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Http;
+
+namespace Cosmic.WebApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+           app.Use(async (context, next) => {
+                await next();
+                if (context.Response.StatusCode == 404 &&
+                    !Path.HasExtension(context.Request.Path.Value) &&
+                    !context.Request.Path.Value.StartsWith("/api/")) {
+                    context.Request.Path = "/index.html";
+                    await next();
+                }
+            });
+
+            var provider = new PhysicalFileProvider(Path.Combine(env.ContentRootPath, "wwwroot", "dist"));
+
+            app.UseDefaultFiles(new DefaultFilesOptions
+            {
+                DefaultFileNames = { "index.html" },
+                FileProvider = provider
+            });
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = provider
+            });
+
+            // Configures application for usage as API
+            // with default route of '/api/[Controller]'
+            app.UseMvcWithDefaultRoute();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            
+            app.UseMvc();
+        }
+
+    }
+}
